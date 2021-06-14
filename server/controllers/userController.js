a7001ffd0045476babd29d57d10d3e7e248b3520
Filipe Silva <filipeqs@outlook.com>
2021-06-14 16:43:45 -0400

    login user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,33 @@ const User = require('../models/userModel');
 const { BadRequest } = require('../utils/errors');
 const generateToken = require('../utils/generateToken');
 
+// @desc    Login User
+// @route   POST api/users/login
+// @access  Public
+const authUser = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) throw new BadRequest('Invalid email or password');
+
+        if (user && (await user.matchPassword(password))) {
+            return res.send({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id),
+            });
+        } else throw new BadRequest('Invalid email or password');
+    } catch (error) {
+        next(error);
+    }
+};
+
+// @desc    Register new User
+// @route   POST api/users
+// @access  Public
 const registerUser = async (req, res, next) => {
     try {
         const { name, email, password } = req.body;
@@ -35,4 +62,4 @@ const registerUser = async (req, res, next) => {
     }
 };
 
-module.exports = { registerUser };
+module.exports = { registerUser, authUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { registerUser } = require('../controllers/userController');
+const { registerUser, authUser } = require('../controllers/userController');
 
 const router = express.Router();
+
+router.route('/login').post(authUser);
 
 router.route('/').post(registerUser);
 

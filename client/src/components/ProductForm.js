c767759de4a3bafc790b72403f181e99eba89005
Filipe Silva <filipeqs@outlook.com>
2021-07-11 16:12:04 -0400

    product form

---
+++ client/src/components/ProductForm.js
@@ -0,0 +1,184 @@
+import React, { useState, Fragment } from 'react';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+
+const ProductForm = ({ error, loading, submitHandler }) => {
+    const [name, setName] = useState('');
+    const [image, setImage] = useState('');
+    const [brand, setBrand] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState(0);
+    const [stock, setStock] = useState([{ count: '', size: '', _id: uuidv4() }]);
+    const [uploading, setUploading] = useState(false);
+
+    const addNewSize = () => setStock([...stock, { count: '', size: '', _id: uuidv4() }]);
+
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('image', file);
+        setUploading(true);
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            };
+
+            const { data } = await axios.post('/api/upload', formData, config);
+            setImage(data);
+            setUploading(false);
+        } catch (error) {
+            console.error(error);
+            setUploading(false);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        submitHandler({
+            name,
+            image,
+            brand,
+            description,
+            price,
+            stock: stock.map(({ count, size }) => ({
+                count,
+                size,
+            })),
+        });
+    };
+
+    return (
+        <Form onSubmit={handleFormSubmit}>
+            {error && <Message variant="danger">{error}</Message>}
+            {loading && <Loader />}
+            <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                    type="name"
+                    placeholder="Enter Name"
+                    value={name}
+                    required
+                    onChange={(e) => setName(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="price">
+                <Form.Label>Price</Form.Label>
+                <Form.Control
+                    type="number"
+                    placeholder="Enter price"
+                    value={price}
+                    required
+                    onChange={(e) => setPrice(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="image">
+                <Form.Label>Image</Form.Label>
+                <Row>
+                    <Col md={8}>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter image URL"
+                            value={image}
+                            required
+                            onChange={(e) => setImage(e.target.value)}
+                        ></Form.Control>
+                    </Col>
+                    <Col md={4}>
+                        <Form.File
+                            id="image-file"
+                            label="Choose File"
+                            custom
+                            onChange={uploadFileHandler}
+                        ></Form.File>
+                    </Col>
+                </Row>
+                {uploading && <Loader />}
+            </Form.Group>
+
+            <Form.Group controlId="brand">
+                <Form.Label>Brand</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter brand"
+                    value={brand}
+                    required
+                    onChange={(e) => setBrand(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="countInStock">
+                {stock.map(({ count, size, _id }) => (
+                    <Fragment key={_id}>
+                        <Row>
+                            <Col>
+                                <Form.Label>Stock</Form.Label>
+                                <Form.Control
+                                    type="number"
+                                    placeholder="Enter count in stock"
+                                    value={count}
+                                    required
+                                    onChange={(e) =>
+                                        setStock(
+                                            stock.map((item) =>
+                                                item._id === _id
+                                                    ? { ...item, count: e.target.value }
+                                                    : item,
+                                            ),
+                                        )
+                                    }
+                                ></Form.Control>
+                            </Col>
+                            <Col>
+                                <Form.Label>Size</Form.Label>
+                                <Form.Control
+                                    type="number"
+                                    placeholder="Enter Size"
+                                    value={size}
+                                    required
+                                    onChange={(e) =>
+                                        setStock(
+                                            stock.map((item) =>
+                                                item._id === _id
+                                                    ? { ...item, size: e.target.value }
+                                                    : item,
+                                            ),
+                                        )
+                                    }
+                                ></Form.Control>
+                            </Col>
+                        </Row>
+                    </Fragment>
+                ))}
+                <Button variant="primary" className="ml-auto" onClick={addNewSize}>
+                    Add Size
+                </Button>
+            </Form.Group>
+
+            <Form.Group controlId="description">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter description"
+                    value={description}
+                    required
+                    onChange={(e) => setDescription(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+
+            <Button type="submit" variant="primary">
+                Create
+            </Button>
+        </Form>
+    );
+};
+
+export default ProductForm;
--- client/src/pages/AdminProductCreateScreen.js
+++ client/src/pages/AdminProductCreateScreen.js
@@ -1,25 +1,13 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Breadcrumb, Container, Form, Button, Row, Col } from 'react-bootstrap';
+import { Breadcrumb, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import Loader from '../components/Loader';
-import axios from 'axios';
-import { v4 as uuidv4 } from 'uuid';
-
-import Message from '../components/Message';
 
 import { createProduct } from '../redux/actions/productActions';
 import { PRODUCT_CREATE_RESET } from '../redux/constants/productConstants';
+import ProductForm from '../components/ProductForm';
 
 const AdminProductCreateScreen = ({ history }) => {
-    const [name, setName] = useState('');
-    const [image, setImage] = useState('');
-    const [brand, setBrand] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState(0);
-    const [stock, setStock] = useState([{ count: '', size: '', _id: uuidv4() }]);
-    const [uploading, setUploading] = useState(false);
-
     const dispatch = useDispatch();
 
     const userLogin = useSelector((state) => state.userLogin);
@@ -40,45 +28,8 @@ const AdminProductCreateScreen = ({ history }) => {
         };
     }, [dispatch]);
 
-    const addNewSize = () => setStock([...stock, { count: '', size: '', _id: uuidv4() }]);
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-        dispatch(
-            createProduct({
-                name,
-                image,
-                brand,
-                description,
-                price,
-                stock: stock.map(({ count, size }) => ({
-                    count,
-                    size,
-                })),
-            }),
-        );
-    };
-
-    const uploadFileHandler = async (e) => {
-        const file = e.target.files[0];
-        const formData = new FormData();
-        formData.append('image', file);
-        setUploading(true);
-
-        try {
-            const config = {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            };
-
-            const { data } = await axios.post('/api/upload', formData, config);
-            setImage(data);
-            setUploading(false);
-        } catch (error) {
-            console.error(error);
-            setUploading(false);
-        }
+    const submitHandler = (product) => {
+        dispatch(createProduct(product));
     };
 
     return (
@@ -95,122 +46,7 @@ const AdminProductCreateScreen = ({ history }) => {
                 </LinkContainer>
                 <Breadcrumb.Item active>Create</Breadcrumb.Item>
             </Breadcrumb>
-            <Form onSubmit={submitHandler}>
-                {error && <Message variant="danger">{error}</Message>}
-                {loading && <Loader />}
-                <Form.Group controlId="name">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                        type="name"
-                        placeholder="Enter Name"
-                        value={name}
-                        required
-                        onChange={(e) => setName(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
-                <Form.Group controlId="price">
-                    <Form.Label>Price</Form.Label>
-                    <Form.Control
-                        type="number"
-                        placeholder="Enter price"
-                        value={price}
-                        required
-                        onChange={(e) => setPrice(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
-
-                <Form.Group controlId="image">
-                    <Form.Label>Image</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter image URL"
-                        value={image}
-                        required
-                        onChange={(e) => setImage(e.target.value)}
-                    ></Form.Control>
-                    <Form.File
-                        id="image-file"
-                        label="Choose File"
-                        custom
-                        onChange={uploadFileHandler}
-                    ></Form.File>
-                    {uploading && <Loader />}
-                </Form.Group>
-
-                <Form.Group controlId="brand">
-                    <Form.Label>Brand</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter brand"
-                        value={brand}
-                        required
-                        onChange={(e) => setBrand(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
-
-                <Form.Group controlId="countInStock">
-                    {stock.map(({ count, size, _id }) => (
-                        <Fragment key={_id}>
-                            <Row>
-                                <Col>
-                                    <Form.Label>Stock</Form.Label>
-                                    <Form.Control
-                                        type="number"
-                                        placeholder="Enter count in stock"
-                                        value={count}
-                                        required
-                                        onChange={(e) =>
-                                            setStock(
-                                                stock.map((item) =>
-                                                    item._id === _id
-                                                        ? { ...item, count: e.target.value }
-                                                        : item,
-                                                ),
-                                            )
-                                        }
-                                    ></Form.Control>
-                                </Col>
-                                <Col>
-                                    <Form.Label>Size</Form.Label>
-                                    <Form.Control
-                                        type="number"
-                                        placeholder="Enter Size"
-                                        value={size}
-                                        required
-                                        onChange={(e) =>
-                                            setStock(
-                                                stock.map((item) =>
-                                                    item._id === _id
-                                                        ? { ...item, size: e.target.value }
-                                                        : item,
-                                                ),
-                                            )
-                                        }
-                                    ></Form.Control>
-                                </Col>
-                            </Row>
-                        </Fragment>
-                    ))}
-                    <Button variant="primary" className="ml-auto" onClick={addNewSize}>
-                        Add Size
-                    </Button>
-                </Form.Group>
-
-                <Form.Group controlId="description">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter description"
-                        value={description}
-                        required
-                        onChange={(e) => setDescription(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
-
-                <Button type="submit" variant="primary">
-                    Create
-                </Button>
-            </Form>
+            <ProductForm error={error} loading={loading} submitHandler={submitHandler} />
         </Container>
     );
 };

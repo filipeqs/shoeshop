0e4d84c420dc0053823d7be8091d9fd549b36b3c
Filipe Silva <filipeqs@outlook.com>
2021-07-14 19:45:55 -0400

    heroku config

--- server/index.js
+++ server/index.js
@@ -26,10 +26,20 @@ app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')),
+    );
+} else {
+    app.get('/', (req, res) => res.send('API is running'));
+}
+
 // Middlewares
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold),

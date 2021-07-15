7f6028562bb13903210503380e8d2eb143adfcd7
Filipe Silva <filipeqs@outlook.com>
2021-07-14 20:52:24 -0400

    loader change

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 
 const Loader = () => {
     return (
-        <div className="loader-container">
-            <object
-                className="loader__img"
-                data="/images/loading.svg"
-                type="image/svg+xml"
-                aria-label="loader"
-            ></object>
-        </div>
+        <Spinner
+            animation="border"
+            role="status"
+            style={{ width: '100px', height: '100px', margin: 'auto', display: 'block' }}
+        >
+            <span className="sr-only">Loading...</span>
+        </Spinner>
     );
 };
 
